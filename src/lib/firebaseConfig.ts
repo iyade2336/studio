f70dcbf71d5349c33804bb3ce06de61f90ee2a83
SILENT AIM <iyadgga@gmail.com>
2025-08-18 00:45:57 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -4,9 +4,9 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://iot-guardian-8o73w.firebaseio.com",
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
