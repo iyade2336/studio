0f5e7cfb114a578594de94e2b4daa8a43076b6e4
SILENT AIM <iyadgga@gmail.com>
2025-08-18 00:52:41 +0000

    Project name IoT Guardian Project ID iot-guardian-8o73w Project number

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -3,10 +3,10 @@
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "iot-guardian-8o73w.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: "iot-guardian-8o73w.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  databaseURL: "https://iot-guardian-8o73w-default-rtdb.europe-west1.firebasedatabase.app",
 };
